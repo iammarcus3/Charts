5bdae267e09e948a55441d98ff66d46900b6c75c
iammarcus3 <marcuswelleon@gmail.com>
2025-09-30 17:49:28 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('App loaded, data available:', artistsYearData, labelPricing);
+  // Rendering logic here (labels, artists, modals, charts, etc.)
+});
