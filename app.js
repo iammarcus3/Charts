f068b0677ec8c032948757b454fb09ac98ff227f
iammarcus3 <marcuswelleon@gmail.com>
2025-09-30 17:46:51 +0200

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('App loaded, data available:', artistsYearData, labelPricing);
-  // Rendering logic here (labels, artists, modals, charts, etc.)
-});
